4a243b29e03b76af92dd5d1168468eca4eaaaafe
gamagrafia <flavio.targaryen@gmail.com>
2021-02-18 01:33:58 -0300

    Adicionei estado ao FORM_Cadastros

--- src/pages/cadastro/cadastro/index.tsx
+++ src/pages/cadastro/cadastro/index.tsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, {useState, FormEvent} from 'react';
 
-import {TitleForm} from './style'
+import {TitleForm,Form} from './style'
 
 const Cadastro: React.FC = () => {
+
+  const [ cpf, setCPF ] = useState('')
+  const [ name, setNOME ] = useState('')
+  const [ userName, setUSERNAME ] = useState('')
+  const [ password, setSENHA ] = useState('')
+  const [ confirmPass, setCONFIRMPASS ] = useState('')
+
+  function criaCadastro( event: FormEvent<HTMLFormElement> ){
+    event.preventDefault()
+  }
+
   return (
+  <>  
+      <div>
       <TitleForm>Peça sua conta e cartão de crédito do Gama Bank </TitleForm>
-      //inserir form//
+      <Form onSubmit={criaCadastro}>
+        <input type= "text" value={''} onChange={ e => setCPF(e.target.value)} placeholder="Digite seu CPF"/>
+        
+        <input type= "text" value={''} onChange={ e => setNOME(e.target.value)} placeholder="Nome Completo"/>
+        
+        <input type= "text" value={''} onChange={ e => setUSERNAME(e.target.value)} placeholder="Nome do usuário"/>
+        
+        <input type= "text" value={''} onChange={ e => setSENHA(e.target.value)} placeholder="Digite sua senha"/>
+        
+        <input type= "text" value={''} onChange={ e => setCONFIRMPASS(e.target.value)} placeholder="Confirme sua senha"/>
+        
+        <button type="submit">Continuar</button>
+      </Form>
+      </div>
+  </>
   );
 }
 
--- src/pages/cadastro/cadastro/style.ts
+++ src/pages/cadastro/cadastro/style.ts
@@ -1,8 +1,15 @@
 import styled from 'styled-components';
 
- export const TitleForm = styled.h1`
+export const TitleForm = styled.h1`
     font-size: 19px;
     color: #1D1D1D;
-
-
+`
+export const Form= styled.form `
+    display:flex;
+    flex-direction: column;
+    justify-content:space-around;
+    background: red;
+    width: 300px;
+    height: 400px;
+    border-radius: 3px;
 `
