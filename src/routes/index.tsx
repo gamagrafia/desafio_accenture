c490aba871ac66bf4ca450f2ce528669b4998499
kakau01 <leite5387@hotmail.com>
2021-02-21 18:11:46 -0300

    feat(routes): ajust settings

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,16 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
-import Login from '../pages/login'
-import Page2 from '../pages/page_2'
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import ErrorPage from '../pages/errorPage';
+import Login from '../pages/login';
+import Page2 from '../pages/page_2';
 
 const Routes: React.FC = () => {
   return (
     <Switch>
       <Route path="/" exact component={Page2} />
       <Route path="/login" component={Login} />
+      <Route path="/error" component={ErrorPage} />
     </Switch>
   );
 };
