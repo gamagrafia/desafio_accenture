456e4112e13bf5d699f23e665d1a0258142c3354
Carol Casagrande <ca_casag@yahoo.com.br>
2021-02-23 14:30:09 -0300

    remoção de arquivos

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,24 +2,18 @@ import React from 'react'
 
 import { Switch, Route } from "react-router-dom";
 
-import PrivateRoute from './privateRoute'
-
 import Home from "../pages/home";
-import Login from "../pages/login";
-import Error from "../pages/error";
+
 import RecoveryPass from "../pages/recoveryPass";
-import DashBoard from "../pages/dashBoard";
+
 
 
 const Routes: React.FC = () => {
     
     return (
         <Switch>
-            <Route path="/" exact component={Home}/>
-            <Route path="/login" component={Login}/>
-            <Route path="/error" component={Error}/>
-            <Route path="/recoveryPass" component={RecoveryPass}/>
-            <PrivateRoute path="/dashboard" component={DashBoard}/>
+            <Route path="/" exact component={Home}/>            
+            <Route path="/recoveryPass" component={RecoveryPass}/>            
         </Switch>
     )
 }
