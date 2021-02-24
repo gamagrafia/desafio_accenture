d180e50133f3d77207775b9741d97646924919ad
kakau01 <leite5387@hotmail.com>
2021-02-24 17:13:55 -0300

    feat(Logo): redirect to home)

--- src/components/logo/index.tsx
+++ src/components/logo/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import logoImg from '../../img/logo.png';
 import { LogoImg } from './styles';
@@ -7,7 +8,9 @@ import { LogoImg } from './styles';
 
 const Logo: React.FC = () => {
   return (
-      <LogoImg src={logoImg} />
+      <Link to="/">
+        <LogoImg src={logoImg} />
+      </Link>
   )
 }
 
