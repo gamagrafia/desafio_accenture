c85ad616bee64d775ece057fb1a637498b971c4d
kakau01 <leite5387@hotmail.com>
2021-02-24 18:04:13 -0300

    feat(routes):PrivateTempPass Route

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -7,6 +7,7 @@ import Home from '../pages/home';
 import Login from '../pages/login';
 import PassRequest from '../pages/passRequest';
 import RecoveryPass from '../pages/recoveryPass';
+import PrivateRecoveryPass from './privateRecoveryPass';
 import PrivateRoute from './privateRoute';
 
 const Routes: React.FC = () => {
@@ -15,7 +16,7 @@ const Routes: React.FC = () => {
       <Route path="/" exact component={Home} />
       <Route path="/login" component={Login} />
       <Route path="/passRequest" component={PassRequest} />
-      <PrivateRoute  path="/recovery" component={RecoveryPass} />
+      <PrivateRecoveryPass path="/recovery" component={RecoveryPass} />
       <Route path="/error" component={ErrorPage} />
       <PrivateRoute path="/dashboard" component={Dashboard} />
     </Switch>
