dcb460e8c33b3c51d44c41d37f7adb0aff2b84b2
kakau01 <leite5387@hotmail.com>
2021-02-24 11:04:06 -0300

    refactor(home): layout responsive

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,24 +1,24 @@
 import React, { FormEvent, useState } from 'react';
+import { FiArrowRight } from 'react-icons/fi';
 import { Link, useHistory } from 'react-router-dom';
 import { toast } from 'react-toastify';
 
 import Logo from '../../components/logo';
-import ArrowGray from '../../img/arrowgray.png';
 import Arrow from '../../img/arrowwhite.png';
 import api from '../../services/api';
-import { Container, Form, SectionOne, SideLeft, SideRight } from './style';
+import { Container, Form, SectionOne, SideLeft } from './style';
 
 const Home: React.FC = () => {
 
   const history = useHistory()
 
-  const [ cpf, setCpf ] = useState('')
-  const [ name, setName ] = useState('')
-  const [ userName, setUserName ] = useState('')
-  const [ password, setPassword ] = useState('')
-  const [ confirmPass, setConfirmPass ] = useState('')
+  const [cpf, setCpf] = useState('')
+  const [name, setName] = useState('')
+  const [userName, setUserName] = useState('')
+  const [password, setPassword] = useState('')
+  const [confirmPass, setConfirmPass] = useState('')
 
-  function createAccount( event: FormEvent<HTMLFormElement> ){
+  function createAccount(event: FormEvent<HTMLFormElement>) {
     event.preventDefault()
 
     const postData = {
@@ -27,21 +27,21 @@ const Home: React.FC = () => {
       login: userName,
       senha: password
     }
-    
-    if ( password !== confirmPass ) {
-      toast.error('Sua senha está incorreta!') 
+
+    if (password !== confirmPass) {
+      toast.error('Sua senha está incorreta!')
       return;
     }
 
     try {
-      api.post(`usuarios`, postData ).then(
-        response => { 
-          if (response.status === 200){
+      api.post(`usuarios`, postData).then(
+        response => {
+          if (response.status === 200) {
             history.push('/login')
           } else {
             toast.error('Algo deu errado, tente novamente em alguns minutos.')
           }
-         }
+        }
       )
     } catch (e) {
       toast.error('algo deu errado')
@@ -50,38 +50,36 @@ const Home: React.FC = () => {
 
   return (
     <>
-    <Logo />
-    <Container>
-      <SectionOne>       
-        <SideLeft>            
-          <h2>Gama Bank é um projeto de nossos alunos.</h2>
-          <h2>Já tem conta?</h2>
-          <Link to="/login">
-            <button>Acessar <img src={Arrow} alt=""/></button>
-          </Link>        
-        </SideLeft>  
-        <SideRight>             
-     
-          <Form onSubmit={createAccount}>  
-          <h4>
-            Peça sua conta e cartão de crédito Gama Bank
-          </h4>             
-            <input type="text"  value={ cpf } onChange={  e => setCpf( e.target.value)  } placeholder="Digite seu CPF"/>
-            <input type="text" value={ name } onChange={  e => setName(e.target.value)  } placeholder="Nome completo"/>
-            <input type="text" value={ userName } onChange={  e =>  setUserName(e.target.value) } placeholder="Nome do usuário"/>
-            <input type="password" value={ password } onChange={  e => setPassword(e.target.value) } placeholder="Digite sua senha"/>
-            <input type="password" value={ confirmPass } onChange={  e => setConfirmPass(e.target.value) } placeholder="Confirme sua senha"/>
-            
+      <Logo />
+      <Container>
+        <SectionOne>
+          <SideLeft>
+            <h2>Gama Bank é um projeto de nossos alunos.</h2>
+            <h2>Já tem conta?</h2>
             <Link to="/login">
-              <button>Continuar <img src={ArrowGray} alt=""/></button>
-            </Link>  
-          </Form>            
-        </SideRight> 
-      </SectionOne>    
-    </Container>
+              <button>Acessar <img src={Arrow} alt="" /></button>
+            </Link>
+          </SideLeft>
+
+          <Form onSubmit={createAccount}>
+            <h4>
+              Peça sua conta e cartão de crédito Gama Bank
+          </h4>
+            <input type="text" value={cpf} onChange={e => setCpf(e.target.value)} placeholder="Digite seu CPF" />
+            <input type="text" value={name} onChange={e => setName(e.target.value)} placeholder="Nome completo" />
+            <input type="text" value={userName} onChange={e => setUserName(e.target.value)} placeholder="Nome do usuário" />
+            <input type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="Digite sua senha" />
+            <input type="password" value={confirmPass} onChange={e => setConfirmPass(e.target.value)} placeholder="Confirme sua senha" />
+
+            <Link to="/login">
+              <button>Continuar <FiArrowRight size={20}  /></button>
+            </Link>
+          </Form>
+        </SectionOne>
+      </Container>
     </>
-    
-    );
+
+  );
 }
 
 export default Home;
--- src/pages/home/style.ts
+++ src/pages/home/style.ts
@@ -1,11 +1,15 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 
-import backgroundImg from "../../img/happyhome.png";
+import backgroundImg from '../../img/happyhome.png';
 
 export const Container = styled.div`
     background-color: #8C52E5;    
     width:100%;
-    //height: 100vh;
+    height: 100vh;
+
+    
+
+
     
 `
 
@@ -16,37 +20,57 @@ export const Header = styled.div`
 
     }
 
-    @media(max-width: 700px){        
+    /* @media(max-width: 700px){        
         width:100vw;
         display: flex;
         justify-content: center;
         align-items:center;
         margin-top: 20px;        
-    }
+    } */
+
 `
 
 export const SectionOne = styled.section`
 
-    padding: 0 135px 0 180px;
-    
+    padding: 0 135px 0 180px;  
     //width:100vw;
+    /* padding: 0px 100px; */
 
     background-image: url(${backgroundImg});
     background-repeat: no-repeat;
     background-position: bottom;
-    background-size: 40%;
+    /* background-size:contain; */
    
-    height: 90vh;
+    height: 100%;
     
     display: flex;
     align-items: center;
     justify-content: space-between;
 
-    @media(max-width: 700px){
+    /* @media(max-width: 700px){
         flex-wrap: wrap;
         justify-content:center;
-        background-image: unset;
-        padding:0;
+        /* background-image: unset; */
+        /* padding:0; */
+    /* } */ 
+
+    @media(max-width: 1301px){
+        padding: 170px 90px;
+
+    }
+    @media(max-width: 1024px){
+        padding: 170px 60px;
+
+    }
+    @media(max-width: 900px){
+      display: flex;
+      flex-direction: column;
+      padding: 170px 100px;
+      width: 100%;
+      
+    }
+    @media(max-width: 480px){
+      
     }
     
 
@@ -54,12 +78,15 @@ export const SectionOne = styled.section`
 
 export const SideLeft = styled.div`
 
+    width: 20%;
+
     h2{
-        width: 280px;
+        
         font-size: 29px;
         color: #fff;
         font-weight: 300;
         margin-bottom: 24px;
+        text-align: left;
     }
 
     button{
@@ -80,14 +107,12 @@ export const SideLeft = styled.div`
         font-weight: 500;
         &:hover{
             background: #68DE5A;
+            
         }
     }
-        img{
-            width:16px;
-            height: 16px;
-        }
+       
 
-        @media(max-width: 700px){
+        /* @media(max-width: 700px){
         margin: 50px 0;
         text-align: center;
 
@@ -96,6 +121,49 @@ export const SideLeft = styled.div`
         align-items: center;
         flex-direction: column;
 
+    } */
+    @media(max-width: 1301px){
+        width: 30%;
+        h2{
+        
+        font-size: 25px;
+        
+        }
+        button {
+            width: 100%;
+        }
+    }
+    @media(max-width: 1024px){
+
+        h2{
+        
+        font-size: 22px;
+        
+        }
+        button {
+            width: 100%;
+        }
+    }
+    @media(max-width: 900px){
+      width: 100%;
+      margin-bottom: 20px;
+      
+      h2{
+          /* display: none; */
+          font-size: 18px;
+          width: 50%;
+          margin-bottom: 10px;
+         
+          
+      }
+      button {
+          width: 40%;
+          margin-bottom: 20px;
+          /* text-align: right; */
+      }
+    }
+    @media(max-width: 480px){
+      
     }
     
 `
@@ -109,41 +177,46 @@ export const Form = styled.form`
     
     background-color:#fff;
     padding: 40px;
-    width: 350px;
+    width:30%;
     border-radius: 8px;
+    display: flex;
+    flex-direction:column;
 
-    h2{
-        font-size:19px;
-        font-weight: 500;
+    h4{
+        font-size:25px;
+        font-weight: 700;
+        margin-bottom: 30px;
+
     }
 
     input{
-        display:block;
+        /* display:block; */
         border: none;
-        font-size: 12px;
+        font-size: 20px;
         padding: 8px;
         font-weight: 300;
         text-align:left;
         width: 100%;
-        margin: 25px 0;
+        margin-bottom: 30px;
         border-bottom: 1px solid #878686;
     }
     
     button{
+        margin-top: 15px;
         display: flex;
         justify-content: space-between;
         align-items: center;
-        cursor: pointer;
+        /* cursor: pointer; */
         border: none;
-        border-radius: 20px;
+        border-radius: 25px;
         color: #9B9B9B;   
-        width: 100%; 
-        padding: 12px 21px;
+        padding: 15px 21px;
         text-align: left;
-        width: 270px;
-        font-size: 12px;
+        width: 100%;
+        font-size: 18px;
         &:hover{
             background: #68DE5A;
+            color: #fff;
         }
     }
 
@@ -152,9 +225,51 @@ export const Form = styled.form`
             height: 16px;
         }
 
-    @media(max-width: 700px){
+    /* @media(max-width: 700px){
     margin-bottom: 50px;
 
+    } */
+    @media(max-width: 1500px){
+        width: 40%;
+
+    }
+    @media(max-width: 1024px){
+        width: 50%;
+        background-color: rgba(243, 243, 243, 0.85);
+
+    }
+    @media(max-width: 900px){
+        width: 100%;
+        padding:20px 20px;
+        background-color: rgba(243, 243, 243, 0.70);
+    
+     h4{
+        font-weight: 700;
+        margin-bottom: 20px;
+        font-size: 1.5rem;
+    }
+
+    input{
+        font-size: 1.0rem;
+        border-width: 0px 0px 1px 0px;
+        padding: 8px;
+        margin-bottom: 15px;
+        border-color: #878686;
+    }
+
+    input + input {
+         margin-top: 0px;
+    }
+    button{
+        margin-top: 13px;
+        padding: 10px;
+            
+           
+        }
+
+    }
+    @media(max-width: 480px){
+      
     }
 
 `
